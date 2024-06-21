e73924dcc829acdcea3a0d3b2c43abaeab51e983
Tarunchauhan100 <tarunthakur21611@gmail.com>
2024-06-21 19:29:27 +0530

    My portfolio Website
    By using html css javascript

---
+++ main.js
@@ -0,0 +1,26 @@
+var typed = new Typed(".text",{
+    strings: ["Frontent Developer", "Web developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
+
+
+var typed = new Typed(".contact-left-title",{
+    strings: ["Contact Me", " Contact Form"],
+    typeSpeed: 90,
+    backSpeed: 90,
+    backDelay: 1000,
+    loop:true
+});
+
+ 
+
+var typed = new Typed(".about-text h4 span",{
+    strings: [" Developer!"],
+    typeSpeed: 110,
+    backSpeed: 110,
+    backDelay: 1000,
+    loop:true
+});
